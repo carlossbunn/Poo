d34a669d29d19d37870d4b187044f08168683cd5
carlossbunn <60108918+carlossbunn@users.noreply.github.com>
2022-05-22 21:09:44 -0300

    Add files via upload

---
+++ enviado prova 1/4/Ponto2D.java
@@ -0,0 +1,12 @@
+public class Ponto2D {
+    private double x,y;
+
+    Ponto2D (double _x,double _y){
+        x= _x;y=_y;
+    }
+}
+/*1) Espaço na criação do metodo "ponto 2D" não se encaixa sintaxe do JAVA
+   o correto seria "Ponto2D".
+2) Na classe Ponto3D precisa ser iniciadas
+   sendo as variais _x,_y,_z não estao inciadas de forma certa.
+2.2) falta o super na herança*/
---
+++ enviado prova 1/4/Ponto3D.java
@@ -0,0 +1,9 @@
+public class Ponto3D extends Ponto2D{
+    private double z;
+    
+    Ponto3D(double x, double y, double z){
+        x=_x;
+        x=_y;
+        x=_z;
+    }
+}
